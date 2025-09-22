1be8f7439b8153e7e1f8b57f8c4f64b2af829ba7
Arsh Vimal <69149458+arshvimal@users.noreply.github.com>
2025-09-22 21:32:51 +0530

    Update zephyrplus-downloads.tsx

--- src/components/dashboard/home/zephyrplus-downloads.tsx
+++ src/components/dashboard/home/zephyrplus-downloads.tsx
@@ -12,11 +12,11 @@ export default function ZephyrPlusDownloads(){
       <CardContent className="space-y-2">
         <Card className="flex items-center h-16 px-4 bg-inherit">
           <div><AppWindow className="mr-4" size={24}/></div>
-          <div><p className="text-xs md:text-sm font-semibold"><Link className="hover:underline" to={"https://github.com/zephiteofficial/vpnet-dashboard/releases/download/beta/ZephyrPlusWindowsInstaller.exe"}>ZephyrPlus Windows Client</Link></p></div>
+          <div><p className="text-xs md:text-sm font-semibold"><Link className="hover:underline" to={"https://github.com/zephiteofficial/vpnet-dashboard/releases/download/beta/ZephyrPlusInstaller.exe"}>ZephyrPlus Windows Client</Link></p></div>
         </Card>
         <Card className="flex items-center h-16 px-4 bg-inherit">
           <div><AppWindow className="mr-4" size={24}/></div>
-          <div><p className="text-xs md:text-sm font-semibold"><Link className="hover:underline" to={"https://github.com/zephiteofficial/vpnet-dashboard/releases/download/beta/ZephyrPlusAndroidClient.apk"}>ZephyrPlus Android Client</Link></p></div>
+          <div><p className="text-xs md:text-sm font-semibold"><Link className="hover:underline" to={"https://github.com/zephiteofficial/vpnet-dashboard/releases/download/beta/ZephyrPlusAndroid.apk"}>ZephyrPlus Android Client</Link></p></div>
         </Card>
       </CardContent>
     </Card>
